3484a47aa3b048bee95bbc62018e03fe11785106
Gabriel de Freitas <gabrielfreitasgarcia@globo.com>
2018-12-19 21:39:32 -0200

    Esqueci de colocar o aux na pasta certa

---
+++ script/aux.js
@@ -0,0 +1,5 @@
+//Este arquivo não faz parte do projeto, apenas criei pra testar as funções enquanto faço
+var lista
+lista = ["batata", "frita"]
+lista.push("é bom")
+console.log(JSON.stringify(lista,null,4))
