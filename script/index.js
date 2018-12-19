e176a10f469db1d52f35c63a545f8f27975ac816
Gabriel de Freitas <gabrielfreitasgarcia@globo.com>
2018-12-19 20:17:48 -0200

    Criei os dois objetos que vou precisar e um arquivo auxiliar que NÃO FAZ PARTE DO PROJETO, criado apenas pra testar funções que estou usando

--- script/index.js
+++ script/index.js
@@ -1,6 +1,21 @@
 /*Nome: Gabriel de Freitas Garcia
 Objetivo: Fazer um programa em node.js que receba um arquivo input.csv, processe os dados e imprima em um arquivo outpu.json*/
-
+function main(){
+  var modelo, endereço
+  modelo = {
+    fullname : "",
+    eid: "",
+    classes : [],
+    addresses: [],
+    invisible: false,
+    see_all: false
+  }
+  endereço = {
+    type: "",
+    tags: [],
+    addres: ""
+  }
+}
 
 
 //Agradeço a Deus porque Ele sempre está comigo, morreu por mim, me salvou e cuidou de mim em cada momento de minha vida.
