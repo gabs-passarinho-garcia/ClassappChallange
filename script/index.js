b6763f139137c10f4789051118e30f70ed67d7bc
Gabriel de Freitas <gabrielfreitasgarcia@globo.com>
2018-12-19 20:03:01 -0200

    Criei o script do projeto

---
+++ script/index.js
@@ -0,0 +1,6 @@
+/*Nome: Gabriel de Freitas Garcia
+Objetivo: Fazer um programa em node.js que receba um arquivo input.csv, processe os dados e imprima em um arquivo outpu.json*/
+
+
+
+//Agradeço a Deus porque Ele sempre está comigo, morreu por mim, me salvou e cuidou de mim em cada momento de minha vida.
