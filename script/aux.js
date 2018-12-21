d026baf5bcff34e26c11703761fa80e397910a5a
Gabriel de Freitas <gabrielfreitasgarcia@globo.com>
2018-12-21 15:51:50 -0200

    Comecei a função para interpretar o header

--- script/aux.js
+++ script/aux.js
@@ -1,5 +1,6 @@
 //Este arquivo não faz parte do projeto, apenas criei pra testar as funções enquanto faço
-var lista
-lista = ["batata", "frita"]
-lista.push("é bom")
-console.log(JSON.stringify(lista,null,4))
+function separar(string){
+  var texto = string.split(/\"|\,/)
+  console.log(JSON.stringify(texto,null,4))
+}
+separar("fullname,eid,class,class,\"email Responsável, Pai\",phone Pai,\"phone Responsável, Mãe\",email Mãe,email Aluno,phone Aluno,invisible,see_all")
