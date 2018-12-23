f11e9169fbf428918ca6c009456510c7c4bd7a98
Gabriel de Freitas <gabrielfreitasgarcia@globo.com>
2018-12-23 01:22:10 -0200

    Terminei o programa, ele está com um erro que não faz sentido

--- script/index.js
+++ script/index.js
@@ -1,39 +1,26 @@
 /*Nome: Gabriel de Freitas Garcia
 Objetivo: Fazer um programa em node.js que receba um arquivo input.csv, processe os dados e imprima em um arquivo outpu.json*/
 function main(){
-  const libphone = require('google-libphonenumber')
   var fs = require("fs")
-  var modelo, endereço, arquivo,linhas,aluno
+  var arquivo,linhas,aluno
   var alunos = []
   var header = []
-  modelo = {
-    fullname : "",
-    eid: "",
-    classes : [],
-    addresses: [],
-    invisible: false,
-    see_all: false
-  }
-  endereço = {
-    type: "",
-    tags: [],
-    address: ""
-  }
   arquivo = fs.readFileSync("input.csv","utf-8")
   linhas = arquivo.split(/\r?\n/)
   header = separar(linhas[0])
   linhas.splice(0,1)
   for (var i = 0; i < linhas.length;i++){
-    aluno = cria_objeto(linhas[i],header,modelo,endereço)
-    alunos.push(aluno)
+    aluno = cria_objeto(linhas[i],header)
+    alunos = coloca(alunos,aluno)
   }
-  console.log(JSON.stringify(alunos,null,4))
+  fs.writeFileSync("output.json",JSON.stringify(alunos,null,1),"utf-8")
+  //console.log(JSON.stringify(alunos,null,4))
 }
 function separar(string){
   var aspas1 = /\".+/, aspas2 = /.+\"/
   var texto = string.split(",")
   for (var i = 0; i < texto.length; i++){
-    if (aspas.test(texto[i])){
+    if (aspas1.test(texto[i])){
       while(true){
         if (i === (texto.length)-1){
           break
@@ -52,24 +39,41 @@ function separar(string){
   }
   return texto
 }
-function cria_objeto(aluno,header,modelo,endereço){
+function cria_objeto(aluno,header){
   const PNF = require('google-libphonenumber').PhoneNumberFormat;
   const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance()
-  var name = /fullname/i, eid = /eid/i, num = /\d+/,classe = /class/i, sala = /Sala \d+/i, correio = /email/i
-  var email = /\w+@\w+\.[a-z]+\.?[a-z]{0,2}/, telefone = /phone/i, invisivel = /invisible/i, ver = /see_all/i
-  aluno = separar(aluno)
-  endereço.tags = []
-  if (aluno[i] === ""){
-    continue
+  var name = /fullname/i, eid = /eid/i, num = /\d+/,classe = /class/i, sala = /sala \d+/i, correio = /email/i
+  var email = /\w+@\w+\.[a-z]+\.?[a-z]{0,2}/, telefone = /phone/i, invisivel = /invisible/i, ver = /see_all/i, letra = /[a-zA-Z]+/,especiais = /[();:!#$*&£\[\]\{\}+=]+/
+  var modelo, endereço
+  modelo = {
+    fullname : "",
+    eid: "",
+    classes : [],
+    addresses: [],
+    invisible: false,
+    see_all: false
   }
+  aluno = separar(aluno)
   for (var i = 0; i < header.length; i++){
+    endereço = {
+      type: "",
+      tags: [],
+      address: ""
+    }
+    endereço.tags = []
+    if (aluno[i] === ""){
+      continue
+    }
     if (name.test(header[i])){
       modelo.fullname = aluno[i]
     } else if (eid.test(header[i])){
       modelo.eid = num.exec(aluno[i])[0]
     } else if (classe.test(header[i])){
-        modelo = adicionar
+        modelo = adicionar(endereço,aluno[i],sala,modelo,1)
     } else if (correio.test(header[i])){
+      if (especiais.test(aluno[i])){
+        continue
+      }
       endereço.type = "email"
       var tags = header[i].split(" ")
       for (var j = 0; j < tags.length; j++){
@@ -79,8 +83,12 @@ function cria_objeto(aluno,header,modelo,endereço){
           endereço.tags.push(tags[j])
         }
       }
-      modelo = adicionar(endereço,aluno[i],email,modelo)
+      console.log("batata")
+      modelo = adicionar(endereço,aluno[i],email,modelo,2)
     } else if (telefone.test(header[i])){
+      if ((letra.test(aluno[i]))){
+        continue
+      }
       endereço.type = "phone"
       var tags = header[i].split(" ")
       for (var j = 0; j < tags.length; j++){
@@ -90,30 +98,74 @@ function cria_objeto(aluno,header,modelo,endereço){
           endereço.tags.push(tags[j])
         }
       }
-      var number = phoneUtil.parseAndKeepRawInput("(11) 38839332", 'BR')
+      var number = phoneUtil.parseAndKeepRawInput(aluno[i], 'BR')
       if (phoneUtil.isValidNumber(number)){
-        endereço.address.push(phoneUtil.format(number, PNF.E164).replace("+",""))
+        endereço.address = phoneUtil.format(number, PNF.E164).replace("+","")
         modelo.addresses.push(endereço)
       }
     } else if (invisivel.test(header[i])){
-      if ((aluno[i] == true || aluno[i] !== "false" || num.test(aluno[i])) && aluno[i] != 0){
+      if ((aluno[i] == true || aluno[i] == "yes" || num.test(aluno[i])) && (aluno[i] != 0 && aluno[i] !== "false" && aluno[i] != "no" && aluno[i] != false)){
         modelo.invisible = true
       }
     } else if (ver.test(header[i])){
-      if ((aluno[i] == true || aluno[i] !== "false" || num.test(aluno[i])) && aluno[i] != 0){
+      if ((aluno[i] == true || aluno[i] == "yes" || num.test(aluno[i])) && (aluno[i] != 0 && aluno[i] !== "false" && aluno[i] != "no" && aluno[i] != false)){
         modelo.see_all = true
       }
     }
   }
+  return modelo
 }
-function adicionar(endereço,aluno,regex,modelo){
+function adicionar(endereço,aluno,regex,modelo,tipo){
   while (regex.test(aluno)){
-    var corresponde = regex.exec(aluno[i])[0]
-    endereço.address = corresponde
-    aluno[i].replace(corresponde,"")
-    modelo.addresses.push(endereço)
+    var corresponde = regex.exec(aluno)[0]
+    console.log(corresponde)
+    if (tipo === 1){
+      modelo.classes.push(corresponde)
+    } else {
+      endereço.address = corresponde
+      modelo.addresses.push(endereço)
+      for (var i = 0; i < modelo.addresses.length; i++){
+        console.log(modelo.addresses[i])
+      }
+    }
+    aluno = aluno.replace(corresponde,"")
   }
   return modelo
+}
+function coloca(lista,aluno){
+  var i ,j
+  for (i = 0; i < lista.length; i++){
+    if (lista[i].eid === aluno.eid){
+      for (j = 0; j < aluno.classes.length; j++){
+        if (!busca(aluno.classes[j],lista[i].classes)){
+          lista[i].classes.push(aluno.classes[j])
+        }
+      }
+      for (j = 0; j < aluno.addresses.length; j++){
+        if (!busca(aluno.addresses[j],lista[i].addresses)){
+          lista[i].addresses.push(aluno.addresses[j])
+        }
+      }
+      if (lista[i].invisible === true || aluno.invisible === true){
+        lista[i].invisible = true
+      }
+      if (lista[i].see_all === true || aluno.see_all === true){
+        lista[i].see_all = true
+      }
+      return lista
+    }
+  }
+  lista.push(aluno)
+  return lista
+}
+function busca(elemento,lista){
+  var i
+  for (i = 0; i < lista.length; i++){
+    if (elemento == lista[i]){
+      return true
+    }
+  }
+  return false
 }
 main()
 //Agradeço a Deus porque Ele sempre está comigo, morreu por mim, me salvou e cuidou de mim em cada momento de minha vida.
